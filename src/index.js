8129486c8ae324994050195af412c96fae14ce14
lasini1022 <102490645+lasini1022@users.noreply.github.com>
2025-05-26 20:41:22 -0400

    search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
+function refreshWeather(reponse) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(reponse.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = reponse.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "14b3e71od6df40977d863a00tdaeb6ef";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=" +
+    city +
+    "&key=" +
+    apiKey;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Toronto");
